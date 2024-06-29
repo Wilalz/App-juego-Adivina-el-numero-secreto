6e4e45598324da13e60d4d4b73e441d0d9a5253d
WilAlz <wilalzatep@gmail.com>
2024-06-29 01:30:25 -0500

    [FEAT] Agregar las funcionalidades con JavaScript y correcciones en CSS

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# App juego Adivina el numero secreto\ Guess the secret number game
+# App juego Adivina el numero secreto
+# Guess the secret number game
 Apliación web para jugar a adivinar un numero secreto.\
 El proyecto tiene un enfoque en el desarrollo BDD y validacion del software (calidad), asi que se inició definiendo los requisitos con historias de usuario y creacion criterios de aceptacion con Gherkin.\
 El desarrollo es con HTML, CSS y JavaScript.
@@ -6,7 +7,7 @@ El desarrollo es con HTML, CSS y JavaScript.
 ## Ver la presentación!
 [Web -> Juego adivina el número secreto](https://wilalz.github.io/App-juego-Adivina-el-numero-secreto/)
   
-  
+
 <!-- imagen -->
 ![app](https://github.com/Wilalz/App-juego-Adivina-el-numero-secreto/blob/4e8b84ca720cd0ed3098c10bc6951bcbb0f92835/Juego%20Adivina%20el%20numero.jpg)
 
---
+++ script.js
@@ -0,0 +1,84 @@
+let nSecreto;
+let btnNuevoJuego = document.getElementById("btn-nuevo-juego");
+let input = document.getElementById("input");
+let btnVerificar = document.getElementById("btn-verificar");
+let mensaje = document.getElementById("mensaje");
+let nUsuario;
+let intentos = 1;
+let palabraIntento = "intento";
+
+function verificar (){
+    if(nSecreto === nUsuario){
+        mensaje.innerText = `¡Lo adivinaste! 🤩 El numero secreto era ${nSecreto}, lo lograste en ${intentos} ${palabraIntento}`;
+        btnNuevoJuego.disabled = false;
+        btnVerificar.disabled = true;
+        input.disabled = true;
+    } else if(nSecreto < nUsuario){
+        definirMensaje("el numero secreto es menor");
+        input.value = "";
+        intentos++;
+        palabraIntento="intentos";
+    } else {
+        definirMensaje("el numero secreto es mayor");
+        input.value = "";
+        intentos++;
+        palabraIntento="intentos";
+    }
+    console.log("intentos: ", intentos)
+}
+
+function nuevoJuego (e) {
+    nSecreto = numeroAleatorio();
+    console.log("numero secreto", nSecreto);
+    input.value = "";
+    btnNuevoJuego.disabled = true;
+    btnVerificar.disabled = false;
+    input.disabled = false;
+    mensaje.innerText = "";
+    intentos = 1;
+
+}
+
+function numeroAleatorio (){
+return Math.floor(Math.random()*10)+1;
+}
+
+function definirMensaje (msg){
+    mensaje.innerText = msg;
+    setTimeout (() => {
+        mensaje.innerText = ""
+    }, 2000)
+}
+
+input.addEventListener("input", (e) => {
+    nUsuario = parseInt(e.target.value);
+    console.log("numero usuario", nUsuario);
+})
+
+btnVerificar.addEventListener("click", verificar);
+
+btnNuevoJuego.addEventListener("click", nuevoJuego);
+
+
+btnNuevoJuego.disabled = false;
+btnVerificar.disabled = true;
+input.disabled = true;
+
+
+// aún sin funcionar
+// if (intentos >= 3){
+
+//     mensaje.innerText = `El numero secreto era ${nSecreto} 😓. ¡Inicia un nuevo juego!`;
+//     btnNuevoJuego.disabled = false;
+//     btnVerificar.disabled = true;
+//     input.disabled = true;
+// }
+
+// aún sin funcionar
+// if(nUsuario < 0 || nUsuario > 10){
+//     definirMensaje("Solo puedes ingresar numeros del 1 al 10")
+//     btnVerificar.disabled = true;
+// }
+
+
+
